71bec57d1106d7ddc43915ecb1fa6e39fbc009f4
9860837383 <117849940+9860837383@users.noreply.github.com>
2023-03-13 12:57:28 +0530

    Delete src directory

--- src/main/java/com/example/storemygoods/repository/IRentingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.storemygoods.repository;
-
-import com.example.storemygoods.model.Renting;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IRentingRepository extends JpaRepository<Renting,Long> {
-}
--- src/main/java/com/example/storemygoods/repository/IStorageUnitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.storemygoods.repository;
-
-import com.example.storemygoods.model.StorageUnit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IStorageUnitRepository extends JpaRepository<StorageUnit,Long> {
-}
